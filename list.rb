402ce7df320edd3d6846dec81d5da15157b4f3ff
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-06-16 15:40:07 +0500

    fix linter errror

--- list.rb
+++ list.rb
@@ -1,18 +1,19 @@
 require_relative 'enumerable'
 class MyList
-    include MyEnumerable
-    def initialize(*items)
-        @list = items
-    end
-    def each(&block)
-        @list.each(&block)
-    end
+  include MyEnumerable
+  def initialize(*items)
+    @list = items
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
 end
 
-list = MyList.new(1,2,3,4)
+list = MyList.new(1, 2, 3, 4)
 p list
-puts list.all? {|e| e < 5}
-puts list.all? {|e| e > 5}
-puts list.any? {|e| e == 2}
-puts list.any? {|e| e == 5}
-print list.filter {|e| e.even?}
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+print(list.filter(&:even?))

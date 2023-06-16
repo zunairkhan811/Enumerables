b05a503c9c0e5a22b61ce2bcef7d92f71ed256bf
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-06-16 14:19:09 +0500

    Verify solutions

--- list.rb
+++ list.rb
@@ -8,3 +8,11 @@ class MyList
         @list.each(&block)
     end
 end
+
+list = MyList.new(1,2,3,4)
+p list
+puts list.all? {|e| e < 5}
+puts list.all? {|e| e > 5}
+puts list.any? {|e| e == 2}
+puts list.any? {|e| e == 5}
+print list.filter {|e| e.even?}

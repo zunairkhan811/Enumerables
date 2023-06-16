46d1262d2be98c0a1fdeb57733f793dd06f78933
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-06-16 13:22:34 +0500

    added MyList class

---
+++ list.rb
@@ -0,0 +1,10 @@
+require_relative 'enumerable'
+class MyList
+    include MyEnumerable
+    def initialize(*items)
+        @list = items
+    end
+    def each(&block)
+        @list.each(&block)
+    end
+end
